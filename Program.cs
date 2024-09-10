c4ef9f14ccbec3a90af6a0402bae4732278c9a44
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-09-10 14:24:12 +0200

    lagt till sortering med både bubblesort och array.sort samt tidtagning på dessa

--- Program.cs
+++ Program.cs
@@ -22,6 +22,34 @@ class Program
     }
 
 
+    static void Main(string[] args)
+    {
+
+        int[] array = RandomArray(100); //skapar en array med 100 tal
+        int[] clonedForBubblesort = (int[])array.Clone();//klon av array för bubblesort
+        int[] clonedForArraysort = (int[])array.Clone();//klon av array för Array.Sort
+
+        Console.WriteLine("Osorterad array");
+        Console.WriteLine(string.Join(", ", array));
+
+        //Mäter tiden för att sortera Bubblesort
+        Stopwatch stopwatch = new Stopwatch(); //nytt tidur
+        stopwatch.Start(); //starta tidur
+        Bubblesort(clonedForBubblesort); //anropar Bubblesort och skickar med clonad data
+        stopwatch.Stop(); //stoppar tidur
+        Console.WriteLine("Sorterad med Bubblesort: ");
+        Console.WriteLine(string.Join(", ", clonedForBubblesort));
+        Console.WriteLine($"Tid att sortera med Bubblesort: {stopwatch.ElapsedMilliseconds} ms");
+
+
+        //Mäter tiden för att sortera Array.Sort
+        stopwatch.Restart(); //nollställer tidur
+        Array.Sort(clonedForArraysort);//sorterar clonad data med inbyggd sortering
+        stopwatch.Stop(); //stoppar tidur
+        Console.WriteLine("\nArray.Sort sorterad array:");
+        Console.WriteLine(string.Join(", ", clonedForArraysort));
+        Console.WriteLine($"Tid att sortera med Array.Sort: {stopwatch.ElapsedMilliseconds} ms");
+    }
     static void Bubblesort(int[] data)
     {
         bool needsSorting = true;
